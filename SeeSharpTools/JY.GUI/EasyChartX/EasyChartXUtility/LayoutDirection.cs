e8fe2885796c3b523fb9720e5437623b25f16fb7
tao.jing <tao.jing@jytek.com>
2019-08-22 11:22:59 +0800

    添加LayoutDirection的注释信息

--- SeeSharpTools/JY.GUI/EasyChartX/EasyChartXUtility/LayoutDirection.cs
+++ SeeSharpTools/JY.GUI/EasyChartX/EasyChartXUtility/LayoutDirection.cs
@@ -1,8 +1,18 @@
 ﻿namespace SeeSharpTools.JY.GUI.EasyChartXUtility
 {
+    /// <summary>
+    /// Split view layout direction
+    /// </summary>
     public enum LayoutDirection
     {
+        /// <summary>
+        /// From left to right
+        /// </summary>
         LeftToRight,
+
+        /// <summary>
+        /// from top to bottom
+        /// </summary>
         TopToBottom,
     }
 }
